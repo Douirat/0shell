e8de2265930934608386df8b798278b21122051c
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2025-11-17 11:17:28 +0100

    handle  multiple commands

--- mini-shell/src/main.rs
+++ mini-shell/src/main.rs
@@ -1,6 +1,6 @@
 mod shell;
 
-use shell::parser::Command;
+ use crate::shell::parser::Commands;
 use std::io::{self, Write};
 
 fn main() {
@@ -11,23 +11,25 @@ fn main() {
         let mut input = String::new();
         io::stdin().read_line(&mut input).unwrap();
 
-        let cmd = Command::parse_command(&input);
+        let commands = Commands::parse_commands(&input);
+        for cmd in commands.commands {
+            // Debug print
+            println!("Command name: {}", cmd.name);
+            println!("Args: {:?}", cmd.args);
 
-        // Debug print
-        println!("Command name: {}", cmd.name);
-        println!("Args: {:?}", cmd.args);
-
-        // Example: simple built-ins
-        match cmd.name.as_str() {
-            "exit" => {
-                println!("Bye!");
-                break;
+            // Example: simple built-ins
+            match cmd.name.as_str() {
+                "exit" => {
+                    println!("Bye!");
+                    break;
+                }
+                "echo" => {
+                    println!("{}", cmd.args.join(" "));
+                }
+                "" => continue, // empty input
+                _ => println!("Unknown command: {}", cmd.name),
             }
-            "echo" => {
-                println!("{}", cmd.args.join(" "));
-            }
-            "" => continue, // empty input
-            _ => println!("Unknown command: {}", cmd.name),
         }
     }
 }
+
--- mini-shell/src/shell/parser.rs
+++ mini-shell/src/shell/parser.rs
@@ -3,6 +3,30 @@ pub struct Command {
     pub args: Vec<String>,
 }
 
+pub struct Commands {
+    pub commands: Vec<Command>,
+}
+
+impl Commands {
+
+    pub fn new(commands: Vec<Command>) -> Self {
+        Commands { commands }
+    }
+
+    pub fn parse_commands(input: &str) -> Commands {
+        /*
+        ----> Split the input by simicolones or new line && operator for multiple commands:
+        */
+        let command_strs: Vec<&str> = input.split(|c| c == ';' || c == '&' || c == '\n').collect();
+        let commands: Vec<Command> = command_strs
+            .iter()
+            .map(|&cmd_str| Command::parse_command(cmd_str))
+            .collect();
+        Commands::new(commands)
+    }
+
+}
+
 impl Command {
     pub fn new(name: String, args: Vec<String>) -> Self {
         Command { name, args }
