da625ac28a9029c3b3b5d0a3c757d7ab110ceae4
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2026-01-15 16:41:40 +0100

    type command plus enter

--- mini_shell/src/main.rs
+++ mini_shell/src/main.rs
@@ -7,7 +7,7 @@ fn main() {
         let mut input = String::new();
         match stdin().read_line(&mut input){
             Ok(_) =>  println!("${}", input.trim()),
-            Err(v) => println!("Error nserting the input"),
+            Err(v) => println!("Error inserting the input"),
         };
     }
 }
