effe4907af85f74573ab0f1afbd0eb775a6c4ebf
aotchoun <aotchoun23@gmail.com>
2026-01-15 20:43:04 +0100

    parser handler

--- mini_shell/src/main.rs
+++ mini_shell/src/main.rs
@@ -1,13 +1,25 @@
 use std::io::*;
+use parser::parse;
 
 fn main() {
     loop{
         print!("$");
         let _=stdout().flush();
+
         let mut input = String::new();
         match stdin().read_line(&mut input){
-            Ok(_) =>  println!("${}", input.trim()),
-            Err(v) => println!("Error inserting the input"),
+            Ok(_) =>  {
+                match parse(&input) {
+                    Ok(command) => {
+                        println!("Command: {}", command.name);
+                        println!("Args: {:?}", command.args);
+                    }
+                    Err(e) => {
+                        println!("Parse error: {}", e);
+                    }
+                }
+            }
+            Err(_) => println!("Error inserting the input"),
         };
     }
 }
