ec774dababaededbd749664a89143a64abd67772
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2026-01-15 15:49:12 +0100

    restructuring the whole project based on crates and modules

--- mini-shell/src/shell/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod parser;
-pub mod executer;
-pub mod executers;
--- mini_shell/executer/executers/src/lib.rs
+++ mini_shell/executer/executers/src/lib.rs
@@ -1,11 +1,11 @@
+// executers/src/lib.rs
+
+pub mod ls;
 pub mod cat;
-pub mod cd;
 pub mod cp;
+pub mod cd;
 pub mod echo;
 pub mod exit;
-pub mod ls;
 pub mod mkdir;
 pub mod mv;
 pub mod pwd;
-pub mod rm;
-
