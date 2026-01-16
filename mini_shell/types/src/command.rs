8db0e59cbabfa0e1e4ea9520426125d4988f7b81
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2026-01-16 10:51:27 +0100

    commands types v2

--- mini_shell/types/src/command.rs
+++ mini_shell/types/src/command.rs
@@ -1,24 +1,25 @@
 // A command enumeration to limit the falling input and redirect the distinction:
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum CommandType{
-echo,
-cd,
-ls, //(supporting -l, -a, -F)
-pwd,
-cat,
-cp,
-rm, // (supporting -r)
-mv,
-mkdir,
-exit,
+Echo,
+Cd,
+Ls, //(supporting -l, -a, -F)
+Pwd,
+Cat,
+Cp,
+Rm, // (supporting -r)
+Mv,
+Mkdir,
+Exit,
 }
 
 // Enumeration to control the flags of ls and rm;
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Flag{
-    l,
-    a,
-    f,
-    r,
+    L, // -l
+    A, // -a
+    F, // -F
+    R, // -r
 }
 
 // Packaging commands togather to ease mutiple command handling:
@@ -31,6 +32,7 @@ pub struct Commands {
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Command {
     pub name: CommandType,
+    pub flags:Vec<Flag>,
     pub args: Vec<String>,
 }
 
