43a407c8865168fa0450b665cf9924f0d41c04c5
aotchoun <aotchoun23@gmail.com>
2026-01-16 13:17:05 +0100

    ajout de la gestion des flags, parser main et command mise à jour

--- mini_shell/src/main.rs
+++ mini_shell/src/main.rs
@@ -1,18 +1,26 @@
 use std::io::*;
 use parser::parse;
+use types::command::CommandType;
 
 fn main() {
-    loop{
-        print!("$");
-        let _=stdout().flush();
+    loop {
+        print!("$ ");
+        let _ = stdout().flush();
 
         let mut input = String::new();
-        match stdin().read_line(&mut input){
-            Ok(_) =>  {
+        match stdin().read_line(&mut input) {
+            Ok(_) => {
                 match parse(&input) {
                     Ok(command) => {
-                        println!("Command: {}", command.name);
+                        println!("Command: {:?}", command.name);
+                        println!("Flags: {:?}", command.flags);
                         println!("Args: {:?}", command.args);
+                        
+                        // Vérifier si c'est la commande exit
+                        if matches!(command.name, CommandType::Exit) {
+                            println!("Goodbye!");
+                            break;
+                        }
                     }
                     Err(e) => {
                         println!("Parse error: {}", e);
