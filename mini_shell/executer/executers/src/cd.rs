f1ebe61671273a244b628d518f56d3e1811450b9
Agiel OTCHOUN <aotchoun23@gmail.com>
2026-01-17 16:46:54 +0100

    ajout de cd et echo

--- mini_shell/executer/executers/src/cd.rs
+++ mini_shell/executer/executers/src/cd.rs
@@ -3,6 +3,28 @@ It allows users to move from the current working directory to another specified 
 This command is essential for exploring different locations within the Linux environment and managing files efficiently.
 */
 use types::command::*;
+use std::env;
+
 pub fn cd(command: &Command){
- println!("--> {:?} --> {:?} --> {:?}", command.name, command.flags, command.args);
+    let path = if command.args.is_empty() {
+        match  env::var("HOME").or_else(|_| env::var("USERPROFILE")) {
+            Ok(home) => home,
+            Err(_) => {
+                eprintln!("cd: HOME directory not found");
+                return;
+            }
+            
+        }
+    } else {
+        command.args[0].clone()
+    };
+
+    match env::set_current_dir(&path) {
+        Ok(_) => {
+
+        }
+        Err(e) => {
+            eprintln!("cd: {}: {}", path, e)
+        }
+    }
 }
