c28c119a8920e6f1c4088048c210ff33ea91d323
Bennacer Douirat <bennacer.douirat.solicode@gmail.com>
2026-01-23 16:45:25 +0100

    single source of truth path

--- mini_shell/types/src/shell_state.rs
+++ mini_shell/types/src/shell_state.rs
@@ -1,4 +1,7 @@
+use std::path::PathBuf;
+
 #[derive(Debug, Eq, PartialmEq)]
 pub struct State{
-
+current_dir: PathBuf, //TODO: remember adding cellRef to all changing even when immutable access is provided:
+previous_dir: Option<PathBuf>, 
 }
