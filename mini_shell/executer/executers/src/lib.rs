f6e31d6a1b83a742fb44c0e1c50541fb5eed15c8
BenDoe <bennacer.douirat.solicode@gmail.com>
2026-02-03 12:59:39 +0100

    rm file/directory

--- mini_shell/executer/executers/src/lib.rs
+++ mini_shell/executer/executers/src/lib.rs
@@ -1,10 +1,10 @@
-pub mod ls; // Ben
+pub mod ls; // Done
 pub mod cat; // TODO: Agiel
 pub mod cp; // TODO: Agiel
-pub mod cd; // Ben
+pub mod cd; // Done
 pub mod echo; // TODO: Agiel
 pub mod exit; // TODO: Agiel
-pub mod mkdir; // Ben
+pub mod mkdir; // Done
 pub mod mv; // TODO: Agiel
 pub mod pwd; // TODO: Agiel
 pub mod rm; // Ben,
--- mini_shell/executer/executers/src/rm.rs
+++ mini_shell/executer/executers/src/rm.rs
@@ -1,5 +1,51 @@
 use types::command::*;
+use std::io::ErrorKind;
+// if dir:
+//     if -r → remove_dir_all
+//     else → error
+// else:
+//     remove_file
+pub fn rm(command: &Command) {
+    if command.args.is_empty() {
+        eprintln!("rm: missing operand");
+        return;
+    }
 
-pub fn rm(command: &Command){
- println!("--> {:?} --> {:?} --> {:?}", command.name, command.flags, command.args);
+    let recursive = command.flags.contains(&Flag::R);
+
+    for arg in &command.args {
+        let path = command.state.cwd.borrow().join(arg);
+
+        match path.symlink_metadata() {
+            Ok(meta) => {
+                if meta.is_dir() {
+                    if recursive {
+                        if let Err(e) = std::fs::remove_dir_all(&path) {
+                            eprintln!("rm: {}: {}", arg, e);
+                        }
+                    } else {
+                        eprintln!("rm: {}: Is a directory", arg);
+                    }
+                } else {
+                    if let Err(e) = std::fs::remove_file(&path) {
+                        eprintln!("rm: {}: {}", arg, e);
+                    }
+                }
+            }
+            Err(e) => {
+                
+            match e.kind() {
+                ErrorKind::NotFound => {
+                    eprintln!("rm: {}: No such file or directory", arg);
+            }
+                ErrorKind::PermissionDenied => {
+                    eprintln!("rm: {}: Permission denied", arg);
+            }
+                _ => {
+                    eprintln!("rm: {}: {}", arg, e);
+            }
+        }
+    }
+}
+}
 }
