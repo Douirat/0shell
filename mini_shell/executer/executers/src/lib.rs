6f880325f0f70c8893637ffc640cf67c6558bd14
BenDoe <bennacer.douirat.solicode@gmail.com>
2026-02-03 14:12:24 +0100

    pwd

--- mini_shell/executer/executers/src/lib.rs
+++ mini_shell/executer/executers/src/lib.rs
@@ -1,10 +1,10 @@
-pub mod ls; // Done
+pub mod ls; // Done by Ben,
 pub mod cat; // TODO: Agiel
 pub mod cp; // TODO: Agiel
-pub mod cd; // Done
+pub mod cd; // Done by Ben,
 pub mod echo; // TODO: Agiel
 pub mod exit; // TODO: Agiel
-pub mod mkdir; // Done
+pub mod mkdir; //Done by Ben,
 pub mod mv; // TODO: Agiel
-pub mod pwd; // Ben
-pub mod rm; // done,
+pub mod pwd; // Done by Ben,
+pub mod rm; // Done by Ben,
